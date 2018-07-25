d46dbe60aea04273ba721cb79e9e853a0662f570
moneroexamples <moneroexamples@tuta.io>
2018-07-25 08:02:53 +0800

    search for tx with same public keys

--- main.cpp
+++ main.cpp
@@ -333,6 +333,12 @@ unordered_map<crypto::public_key, tuple<uint64_t, vector<uint64_t>>> ring_member
 vector<crypto::key_image> key_images_gen;
 
 size_t blk_counter {0};
+size_t same_pubkey_tx_counter {0}; // no of tx with same duplicate pubkeys
+
+auto same_pubkey_txs = csv::ofstream {"same_pubkey_txs.csv"};
+
+same_pubkey_txs << "index" << "timestamp" << "block" << "tx_hash"
+                << "no_of_same_pubkeys" << NEWLINE;
 
 for (uint64_t i = start_height; i < height; ++i)
 {
@@ -401,292 +407,73 @@ for (uint64_t i = start_height; i < height; ++i)
 
 
         // get tx public key from extras field
-        crypto::public_key pub_tx_key = xmreg::get_tx_pub_key_from_received_outs(tx);
+        crypto::public_key pub_tx_key = xmreg::get_tx_pub_key_from_received_outs(tx);                
 
+        std::vector<cryptonote::tx_extra_field> tx_extra_fields;
 
-        if (!found_outputs.empty())
+        string tx_hash_str = epee::string_tools::pod_to_hex(tx_hash);
+
+        if(!parse_tx_extra(tx.extra, tx_extra_fields))
         {
-            print(" - found {:02d} outputs in block {:08d} ({:s}) - writing to the csv\n",
-                  found_outputs.size(), i, blk_time);
-
-            // save found our outputs to the csv file
-            for (auto& tr_details: found_outputs)
-            {
-
-                if (SPEND_KEY_GIVEN)
-                {
-                    // before output details are saved in csv,  lets
-                    // generate its key image using our private spend key (if given)
-                    // and save it in global vector key_images_gen
-                    // the key_image generated is saved in the csv file
-
-                    // public tx key is combined with our private view key
-                    // to create, so called, derived key.
-                    // the derived key is used to produce the key_image
-                    // that we want.
-                    crypto::key_derivation derivation;
-
-                    if (!generate_key_derivation(pub_tx_key, prv_view_key, derivation))
-                    {
-                        cerr << "Cant get derived key for output with: \n"
-                             << "pub_tx_key: " << prv_view_key << '\n';
-                        return EXIT_FAILURE;
-                    }
-
-                    // generate key_image of this output
-                    crypto::key_image key_img;
-
-                    if (!xmreg::generate_key_image(derivation,
-                                                   tr_details.m_internal_output_index, /* position in the tx */
-                                                   prv_spend_key,
-                                                   account_keys.m_account_address.m_spend_public_key,
-                                                   key_img))
-                    {
-                        cerr << "Cant generate key image for output: "
-                             << tr_details.out_pub_key << '\n';
-                        return EXIT_FAILURE;
-                    }
-
-                    cout << "   - output pub key: " << tr_details.out_pub_key
-                         << ",  key image: " << key_img << '\n';
-
-                    key_images_gen.push_back(key_img);
-
-                    // copy key_image to tr_details to be saved
-                    tr_details.key_img = key_img;
-
-                    // check if output was spent
-                    tr_details.m_spent = core_storage->have_tx_keyimg_as_spent(key_img);
-
-                  } // if (SPEND_KEY_GIVEN)
-
-                  if (all_outputs == false)
-                  {
-                      // store public key and amount of output that belongs to us
-                      known_outputs_keys.push_back({tr_details.out_pub_key, tr_details.amount()});
-                  }
-
-                  *csv_os << tr_details << NEWLINE;
-
-                  csv_os->flush();
-
-            } // for (const auto& tr_details: found_outputs)
-
-        } // if (!found_outputs.empty())
-
-        // we finished checking outputs in a tx
-        // thus check for inputs, if spend key was given or
-        // we want to check only if our outputs were used
-        // as ring members somewhere
-
-        // get the total number of inputs in a transaction.
-        // some of these inputs might be our spendings
-        size_t input_no = tx.vin.size();
-
-        if (all_key_images && input_no > 0)
-        {
-            if(tx.vin[0].type() == typeid(cryptonote::txin_to_key))
-                print(" - found {:02d}  inputs in block {:08d} ({:s}) - writing to the csv\n",
-                      input_no, i, blk_time);
+          // Extra may only be partially parsed, it's OK if tx_extra_fields contains public key
+          cerr << "Transaction extra has unsupported format: "
+               << tx_hash_str << '\n';
+          continue;
         }
 
-        //cout << tx_hash << ", input_no " << input_no << endl;
+        std::unordered_set<crypto::public_key> public_keys_seen;
 
-        for (size_t ii = 0; ii < input_no; ++ii)
+        size_t pk_index = 0;
+
+        size_t no_of_same_pubkeys {1};
+
+        while (!tx.vout.empty())
         {
+            cryptonote::tx_extra_pub_key pub_key_field;
 
-            if(tx.vin[ii].type() != typeid(cryptonote::txin_to_key))
-                continue;
-
-            //cout << tx_hash << endl;
-            //cout << "checkint inputs:" << ii << "/" << input_no << endl;
-
-            // get tx input key
-            const cryptonote::txin_to_key& tx_in_to_key
-                    = boost::get<cryptonote::txin_to_key>(tx.vin[ii]);
-
-            // check if the public key image of this input
-            // matches any of your key images that were
-            // generated for every output that we received
-            std::vector<crypto::key_image>::iterator it;
-
-            it = find(key_images_gen.begin(), key_images_gen.end(),
-                      tx_in_to_key.k_image);
-
-            //cout << "Input no: " << ii << ", " << tx_in_to_key.k_image;
-
-            uint64_t xmr_amount = tx_in_to_key.amount;
-
-            bool our_key_image = (it != key_images_gen.end());
-
-            if (our_key_image)
+            if(!find_tx_extra_field_by_type(tx_extra_fields, pub_key_field, pk_index++))
             {
-                cout << " - found our input: " << ", " << tx_in_to_key.k_image
-                     << ", amount: " << cryptonote::print_money(tx_in_to_key.amount)
-                     << '\n';
+              if (pk_index > 1)
+                break;
+
+              cerr << "Public key wasn't found in the transaction extra. Skipping transaction "
+                   << tx_hash_str << '\n';
+              break;
             }
 
-            if ((ring_members && !our_key_image) || all_key_images)
+            if (public_keys_seen.find(pub_key_field.pub_key) != public_keys_seen.end())
             {
-                // search if any of the outputs
-                // have been used as a ring member.
-                // this will include our own key images
-                // we also use this if statment when all_key_images option is used
-                //
 
+              ++no_of_same_pubkeys;
+            }
 
-                // from  bool core::check_tx_inputs_keyimages_domain(const transaction& tx) const
-                if (!(rct::scalarmultKey(rct::ki2rct(tx_in_to_key.k_image),
-                                         rct::curveOrder()) == rct::identity()))
-                {
-                    cerr << "Found key image with wrong domain: "
-                         << epee::string_tools::pod_to_hex(tx_in_to_key.k_image)
-                         << " in tx: " << epee::string_tools::pod_to_hex(tx_hash)
-                         << endl;
-                    return EXIT_SUCCESS;
-                }
+            public_keys_seen.insert(pub_key_field.pub_key);
+        }
 
-                // get absolute offsets of mixins
-                std::vector<uint64_t> absolute_offsets
-                        = cryptonote::relative_output_offsets_to_absolute(
-                                tx_in_to_key.key_offsets);
+        if (no_of_same_pubkeys > 1)
+        {
+            cout << "  - " << (++same_pubkey_tx_counter)
+                 << " the same tx pubkey is present more than once ("
+                 << no_of_same_pubkeys << " times) in block / timestamp / tx: "
+                 << i << " / " << xmreg::timestamp_to_str(blk.timestamp)
+                 << " / " << tx_hash_str << '\n';
 
-                // get public keys of outputs used in the mixins that match to the offests
-                std::vector<cryptonote::output_data_t> mixin_outputs;
+            same_pubkey_txs << same_pubkey_tx_counter
+                            << xmreg::timestamp_to_str(blk.timestamp)
+                            << i<< tx_hash_str
+                            << no_of_same_pubkeys
+                            << NEWLINE;
 
-                try
-                {
-                    core_storage->get_db().get_output_key(xmr_amount,
-                                                          absolute_offsets,
-                                                          mixin_outputs);
-                }
-                catch (const cryptonote::OUTPUT_DNE& e)
-                {
-                    cerr << "Mixins key images not found" << '\n';
-                    continue;
-                }
-
-                uint64_t ring_size = absolute_offsets.size();
-
-                // mixin counter
-                size_t count = 0;
-
-                // for each found output public key check if its ours or not
-                for (const uint64_t& abs_offset: absolute_offsets)
-                {
-                    // get basic information about mixn's output
-                    cryptonote::output_data_t output_data = mixin_outputs.at(count);
-
-                    // before going to the mysql, check our known outputs cash
-                    // if the key exists. Its much faster than going to mysql
-                    // for this.
-
-                    auto it =  std::find_if(
-                            known_outputs_keys.begin(),
-                            known_outputs_keys.end(),
-                            [&](const pair<crypto::public_key, uint64_t>& known_output)
-                            {
-                                return output_data.pubkey == known_output.first;
-                            });
-
-                    if (all_key_images)
-                    {
-                        // write all key images into file
-                        // with associated offsets and the references public
-                        // output keys in this format
-
-                        // get reference output tx (i.e., from which tx the output comes from)
-                        cryptonote::tx_out_index tx_out_idx;
-
-                        try
-                        {
-                            // get pair pair<crypto::hash, uint64_t> where first is tx hash
-                            // and second is local index of the output i in that tx
-                            tx_out_idx = core_storage->get_db()
-                                    .get_output_tx_and_index(xmr_amount, abs_offset);
-                        }
-                        catch (const cryptonote::OUTPUT_DNE &e)
-                        {
-
-                            string out_msg = fmt::format(
-                                    "Output with amount {:d} and index {:d} does not exist!",
-                                    xmr_amount, abs_offset);
-
-                            cerr << out_msg << '\n';
-
-                            ++count;
-                            continue;
-                        }
-
-                        *csv_os4 << blk_time << i << epee::string_tools::pod_to_hex(tx_hash)
-                                 << epee::string_tools::pod_to_hex(tx_in_to_key.k_image)
-                                 << ring_size
-                                 << abs_offset
-                                 << epee::string_tools::pod_to_hex(output_data.pubkey)
-                                 << epee::string_tools::pod_to_hex(tx_out_idx.first)
-                                 << tx_out_idx.second
-                                 << NEWLINE;
-
-                        ++count;
-                        continue;
-                    }
-
-                    if (it == known_outputs_keys.end())
-                    {
-                        // this mixins's output is unknown.
-                        ++count;
-                        continue;
-                    }
-
-                    // this seems to be our mixin.
-
-                    std::get<0>(ring_member_frequency[it->first]) += 1;
-                    std::get<1>(ring_member_frequency[it->first]).push_back(ring_size);
-
-                    cout << " - found output as ring member: " << (count + 1)
-                         << ", " << it->first
-                         << ", tx hash: " << tx_hash << '\n';
-
-                    *csv_os2 << blk_time << i
-                             << epee::string_tools::pod_to_hex(tx_hash)
-                             << epee::string_tools::pod_to_hex(it->first)
-                             << epee::string_tools::pod_to_hex(tx_in_to_key.k_image)
-                             << std::to_string(count + 1) + "/" + std::to_string(ring_size)
-                             << NEWLINE;
-
-                    csv_os2->flush();
-
-                    ++count;
-
-                } // for (const cryptonote::output_data_t& output_data: outputs)
-
-            } //else if (it != key_images_gen.end())
-
-        } // for (size_t ii = 0; ii < input_no; ++ii)
+            same_pubkey_txs.flush();
+        }
 
     } // for (const cryptonote::transaction& tx : txs)
 
 } // for (uint64_t i = 0; i < height; ++i)
 
-if (csv_os->is_open())
-{
-    csv_os->close();
-    cout << "\nCsv saved as: " << out_csv_file << '\n';
-}
+same_pubkey_txs.close();
+cout << "\nCsv saved as: ./same_pubkey_txs.csv\n";
 
-
-if (ring_members && csv_os2->is_open())
-{
-    cout << "\nRing members csv saved as: " << out_csv_file2 << '\n';
-    csv_os2->close();
-}
-
-
-if (all_key_images && csv_os4->is_open())
-{
-    cout << "\nRing members csv saved as: " << out_csv_file4 << '\n';
-    csv_os4->close();
-}
 
 
 // set timezone to orginal value
@@ -696,76 +483,6 @@ if (tz_org != 0)
     tzset();
 }
 
-
-
-
-if (ring_members)
-{
-
-    // we need to sort the frequencies from most frequent to lease
-    // frequent. For this, <public_key, freq> values from map
-    // are copied to vector, which is then sorted by freq.
-    // since I dont want to kep passing the pairs by value 2 times
-    // (map->vector->sorting), I just keep pointers to map pairs
-    // in vector and operate on the pointers only.
-
-    using map_ptr_t = decltype(ring_member_frequency)::value_type const*;
-
-    vector<map_ptr_t> sorted_frequencies;
-    sorted_frequencies.reserve(ring_member_frequency.size());
-
-    for (auto const& kvp: ring_member_frequency)
-        sorted_frequencies.push_back(&kvp);
-
-
-    std::sort(sorted_frequencies.begin(), sorted_frequencies.end(),
-              [](map_ptr_t const& left, map_ptr_t const&  right)
-              {
-                  // types look complicated. so 'left' is
-                  // referenece to 'const pointer' which points to 'const map_t'
-                  return std::get<0>(left->second) > std::get<0>(right->second);
-              });
-
-
-    unique_ptr<csv::ofstream> csv_os3(new csv::ofstream {out_csv_file3.c_str()});
-
-    if (!csv_os3->is_open())
-        cerr << "Cant open file: " << out_csv_file3 << endl;
-
-    // write the header of the csv file to be created
-    *csv_os3 << "Output_pub_key" << "Frequency" << "Ring_size" << NEWLINE;
-
-    cout << "\nMost frequent outputs used as ring members are:\n";
-
-    size_t i {0};
-
-    for (map_ptr_t& kvp: sorted_frequencies)
-    {
-        string ring_sizes = vec2str(std::get<1>(kvp->second), "_");
-
-        if (++i < 10) // dont show more than ten. all of them are in output csv.
-            cout << " - "
-                 << kvp->first /* output public key */
-                 << ": "
-                 << std::get<0>(kvp->second) /* frequency */
-                 << " times with ring sizes of "
-                 << ring_sizes  /* ring sizes */
-                 << '\n';
-
-        if  (csv_os3->is_open())
-            *csv_os3 << epee::string_tools::pod_to_hex(kvp->first) /* output public key */
-                     << std::get<0>(kvp->second) /* frequency */
-                     << ring_sizes  /* ring sizes */
-                     << NEWLINE;
-    }
-
-    if (csv_os3->is_open())
-    {
-        cout << "\nFrequency of outputs as ring members csv saved as: " << out_csv_file3 << '\n';
-        csv_os3->close();
-    }
-
-} // if (ring_members)
 
 
 cout << "\nEnd of program." << '\n';
