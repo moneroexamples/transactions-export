58a491edf604aa72a04dfa982229be0907bb036a
moneroexamples <moneroexamples@tuta.io>
2019-12-08 18:47:19 +0800

    Fix: compilation with latest monero
    https://github.com/moneroexamples/transactions-export/issues/18

--- cmake/FindMonero.cmake
+++ cmake/FindMonero.cmake
@@ -31,7 +31,7 @@
 set(LIBS common;blocks;cryptonote_basic;cryptonote_core;
 		cryptonote_protocol;daemonizer;mnemonics;epee;lmdb;device;
                 blockchain_db;ringct;wallet;cncrypto;easylogging;version;
-                checkpoints)
+                checkpoints;randomx;hardforks)
 
 set(Xmr_INCLUDE_DIRS "${CPP_MONERO_DIR}")
 
@@ -45,7 +45,7 @@ foreach (l ${LIBS})
 	find_library(Xmr_${L}_LIBRARY
 			NAMES ${l}
 			PATHS ${CMAKE_LIBRARY_PATH}
-			PATH_SUFFIXES "/src/${l}" "/src/" "/external/db_drivers/lib${l}" "/lib" "/src/crypto" "/contrib/epee/src" "/external/easylogging++/"
+			PATH_SUFFIXES "/src/${l}" "/src/" "/external/db_drivers/lib${l}" "/lib" "/src/crypto" "/contrib/epee/src" "/external/easylogging++/" "/external/${l}"
 			NO_DEFAULT_PATH
 			)
 
@@ -83,4 +83,4 @@ include_directories(
 		${MONERO_SOURCE_DIR}/external/easylogging++
 		${MONERO_SOURCE_DIR}/contrib/epee/include
 		${MONERO_SOURCE_DIR}/version
-                ${MONERO_SOURCE_DIR}/external/db_drivers/liblmdb)
+        ${MONERO_SOURCE_DIR}/external/db_drivers/liblmdb)
